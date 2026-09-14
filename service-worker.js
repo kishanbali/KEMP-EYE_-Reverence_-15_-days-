c6d2efd091420def35085e2832ba55f5249ae6d5
KishanBali <kishansaraiya9@gmail.com>
2026-09-14 12:51:01 +0530

    Refresh PWA service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kemp-reverberance-pwa-v2';
+const CACHE='kemp-reverberance-pwa-v3';
 const CORE=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
